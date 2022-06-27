4f8061b15a9bebf669972a58b69d368bbc87535e
GustavoGMachado <gustavogmachado2002@gmail.com>
2022-06-27 17:15:20 -0300

    pre-style done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,49 @@
 const pizzaArea = document.querySelector('.pizzaArea')
 const menuArea = document.querySelector('.menuArea')
 
+//map rodando todo o vetor de pizzas.js
 pizzaJson.map((item, index) => {
     const pizzaAreaItem = pizzaArea.cloneNode(true)
 
+    //colocando os valores na pag inicial
     pizzaAreaItem.setAttribute('key', index)
     pizzaAreaItem.querySelector(".pizzaArea-areaImg img").src = item.img
     pizzaAreaItem.querySelector('#pizzaArea-price').innerHTML = `R$ ${item.price[2].toFixed(2)}`
     pizzaAreaItem.querySelector('#pizzaArea-name').innerHTML = item.name
     pizzaAreaItem.querySelector('#pizzaArea-description').innerHTML = item.description
+    
+    //colocando valores na windowPizza
     pizzaAreaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault()
         let key = e.target.closest('.pizzaArea').getAttribute('key')
 
-        //colocar valores na windowPizza
         document.querySelector('.box1-img img').src = pizzaJson[key].img
         document.querySelector('.box1-description .description-title').innerHTML = pizzaJson[key].name
         document.querySelector('.box1-description .description-text').innerHTML = pizzaJson[key].description
+
+        //remover o selected caso ele esteja em outra div que não seja a do size "grande"
+        document.querySelector('.sizeLineText.selected').classList.remove('selected')
+
+        const sizeLineText = document.querySelectorAll('.sizeLineText')
+        sizeLineText.forEach((itSizeText, ind) => {
+            //setando tamanho grande por padrão
+            if(ind === 2) {
+                itSizeText.classList.add('selected')
+            }
+        })
+        //conferir valores depois, pensar na ideida de juntar, aproveitar o forEach da sizeLine no priceLine
+        const sizeLine = document.querySelectorAll('.sizeLine')
+        sizeLine.forEach((itSize, ind) => {
+            itSize.innerHTML = pizzaJson[key].sizes[ind]
+        })
+        const priceLine = document.querySelectorAll('.priceLine')
+        priceLine.forEach((itPrice, ind) => {
+            itPrice.innerHTML = `R$ ${pizzaJson[key].price[ind].toFixed(2)}`            
+        })
+
+     
+
+
 
 
         
--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -1,6 +1,6 @@
 let pizzaJson = [
-    {id: 1, name:'Mussarela', img:'assets/img/pizza.png', price:[15.00, 25.00, 30.00], sizes:['100g', '530g', '860g'], description:'Mussarela, palmito, azeitonas, pepperoni, presunto, milho, orégano e ovo'},
-    {id: 2, name:'Calabresa', img:'assets/img/pizza2.png', price:[15.00, 25.00, 30.00], sizes:['320g', '530g', '860g'], description:'Mussarela, palmito, azeitonas, pepperoni, presunto, milho, orégano e ovo'},
+    {id: 1, name:'Mussarela', img:'assets/img/pizza.png', price:[15.00, 28.00, 30.00], sizes:['100g', '530g', '860g'], description:'Mussarela, palmito, azeitonas, pepperoni, presunto, milho, orégano e ovo'},
+    {id: 2, name:'Calabresa', img:'assets/img/pizza2.png', price:[15.00, 25.00, 35.00], sizes:['320g', '530g', '860g'], description:'Mussarela, palmito, azeitonas, pepperoni, presunto, milho, orégano e ovo'},
     {id: 3, name:'Quatro Queijos', img:'assets/img/pizza3.png', price:[15.00, 25.00, 30.00], sizes:['320g', '530g', '860g'], description:'Mussarela, palmito, azeitonas, pepperoni, presunto, milho, orégano e ovo'},
     {id: 4, name:'Americana', img:'assets/img/pizza4.png', price:[15.00, 25.00, 30.00], sizes:['320g', '530g', '860g'], description:'Mussarela, palmito, azeitonas, pepperoni, presunto, milho, orégano e ovo'},
     {id: 5, name:'Sorvete', img:'assets/img/pizza5.png', price:[15.00, 25.00, 30.00], sizes:['320g', '530g', '860g'], description:'Mussarela, palmito, azeitonas, pepperoni, presunto, milho, orégano e ovo'},
